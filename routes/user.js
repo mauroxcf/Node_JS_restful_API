06c55f9d0efecff70a197ac1c856ba2fa10be232
mauroxcf <maurox03@gmail.com>
2021-06-10 18:21:58 -0500

    estableciendo metodos y validaciones en las rutas o endpoints del proyecto

--- routes/user.js
+++ routes/user.js
@@ -1,3 +1,69 @@
 const express = require('express');
+const jwt = require('jsonwebtoken');
 const router = express.Router();
+
+//Import MongoDB models
+const User = require('../models/User')
+
+//Validation import
+const { registerValidation, loginValidation } = require('../validation');
+
+// using the router method post to register a user
+router.post('/register', async (req, res) => {
+
+	// Validate User
+	const { error } = registerValidation(req.body);
+	if (error) return res.status(400).send(error.details[0].message);
+
+	// Check if User already in database
+	const emailExist = await User.findOne({ email: req.body.email });
+	if(emailExist) return res.status(400).send('Email already exists');
+
+	// Validated And Create User
+	const user = new User({
+		name: req.body.name,
+		email: req.body.email
+	});
+
+	try{
+		const savedUser = await user.save();
+		res.json({ name: user.name, email: user.email });
+	} catch (err) {
+		res.json({ message: err });
+	}
+});
+
+// Login User
+router.post('/login', async(req, res) => {
+	const { error } = loginValidation(req.body);
+	if (error) return res.status(400).send(error.details[0].message);
+
+	// Check if Email Exists
+	const user = await User.findOne({ email: req.body.email });
+	if(!user) return res.status(400).send('Email Does Not Exist');
+
+	// Create & Assign Token
+	const token = jwt.sign({ _id: user._id }, process.env.TOKEN_SECRET);
+	res.header('auth-token', token).send(token);
+});
+
+// Delete user
+router.delete('/:uid/only', async(req, res) => {
+	try {
+		const removedUser = await User.remove({ _id: req.params.uid });
+		res.json(removedUser);
+	} catch(err) {
+		res.json({ message: err });
+	}
+});
+
+router.delete('/:uid/all', async(req, res) => {
+	try {
+		const removedUser = await User.remove({ _id: req.params.uid, get: req.body });
+		res.json(removedUser);
+	} catch(err) {
+		res.json({ message: err });
+	}
+});
+
 module.exports = router;
---
+++ validation.js
@@ -0,0 +1,25 @@
+const Joi = require('joi');
+
+// validate the fields of the created object
+//validate the fields in registerValidation
+const registerValidation = data => {
+	const RegistrationSchema = Joi.object({
+		name: Joi.string().min(6).required(),
+		email: Joi.string().min(6).required().email(),
+	});
+
+	return RegistrationSchema.validate(data);
+};
+
+//validate the fields in registerValidation
+const loginValidation = data => {
+	const LoginSchema = Joi.object({
+        name: Joi.string().min(6).required(),
+		email: Joi.string().min(6).required().email()
+	});
+
+	return LoginSchema.validate(data);
+};
+
+module.exports.loginValidation = loginValidation;
+module.exports.registerValidation = registerValidation;
